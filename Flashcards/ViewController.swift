31130e36b65941d6da83eee11507946694427314
w2huy <huynhwi2@msu.edu>
2020-02-22 17:33:48 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by William Huynh on 2/22/20.
+//  Copyright © 2020 wi2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
